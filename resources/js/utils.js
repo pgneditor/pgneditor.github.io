2ed81c1b0caabcc034a31ce9c3e037c7ccc4027f
pgneditor <pgneditorapplication@gmail.com>
2020-01-07 01:31:09 +0100

    Order editable list

--- resources/js/utils.js
+++ resources/js/utils.js
@@ -1,7 +1,15 @@
-const e = React.createElement
+Array.prototype.itoj = function(i, j){    
+    while(i != j){
+        const n = j > i ? i + 1 : i - 1;
+        [ this[i], this[n] ] = [ this[n], this[i] ]
+        i = n
+    }
+}
 
 /////////////////////////////////////////////////////
 // widget utils
+const e = React.createElement
+
 class ComponentProps_{
     constructor(objopt){
         this.obj = objopt || {}
@@ -13,30 +21,34 @@ class ComponentProps_{
         return this
     }
 
-    w(x){this.addstyle("width", x + "px");return this}
-    miw(x){this.addstyle("minWidth", x + "px");return this}
-    maw(x){this.addstyle("maxWidth", x + "px");return this}
+    w(x){return this.addstyle("width", x + "px")}
+    miw(x){return this.addstyle("minWidth", x + "px")}
+    maw(x){return this.addstyle("maxWidth", x + "px")}
     ww(x){return this.miw(x).maw(x)}
-    h(x){this.addstyle("height", x + "px");return this}
-    mih(x){this.addstyle("minHeight", x + "px");return this}
-    mah(x){this.addstyle("maxHeight", x + "px");return this}
+    h(x){return this.addstyle("height", x + "px")}
+    mih(x){return this.addstyle("minHeight", x + "px")}
+    mah(x){return this.addstyle("maxHeight", x + "px")}
     hh(x){return this.mih(x).mah(x)}
-    t(x){this.addstyle("top", x + "px");return this}
-    l(x){this.addstyle("left", x + "px");return this}
-    bc(x){this.addstyle("backgroundColor", x);return this}
-    mar(x){this.addstyle("margin", x + "px");return this}
-    pad(x){this.addstyle("padding", x + "px");return this}
-    disp(x){this.addstyle("display", x);return this}
+    t(x){return this.addstyle("top", x + "px")}
+    l(x){return this.addstyle("left", x + "px")}
+    bc(x){return this.addstyle("backgroundColor", x)}
+    mar(x){return this.addstyle("margin", x + "px")}
+    marl(x){return this.addstyle("marginLeft", x + "px")}
+    marr(x){return this.addstyle("marginRight", x + "px")}
+    pad(x){return this.addstyle("padding", x + "px")}
+    padl(x){return this.addstyle("paddingLeft", x + "px")}
+    padr(x){return this.addstyle("paddingRight", x + "px")}
+    disp(x){return this.addstyle("display", x)}
     show(x){return this.disp(x ? "initial" : "none")}
-    dib(){this.addstyle("display", "inline-block");return this}
-    fd(x){this.addstyle("flexDirection", x);return this}
-    ai(x){this.addstyle("alignItems", x);return this}    
+    dib(){return this.addstyle("display", "inline-block")}
+    fd(x){return this.addstyle("flexDirection", x)}
+    ai(x){return this.addstyle("alignItems", x)}    
     dfc(){return this.disp("flex").ai("center")}
     dfcc(){return this.disp("flex").fd("column").ai("center")}
-    ff(x){this.addstyle("fontFamily", x);return this}
-    fs(x){this.addstyle("fontSize", x + "px");return this}
+    ff(x){return this.addstyle("fontFamily", x)}
+    fs(x){return this.addstyle("fontSize", x + "px")}
     ffm(){return this.ff("monospace")}
-    pos(x){this.addstyle("position", x);return this}
+    pos(x){return this.addstyle("position", x)}
     por(){return this.pos("relative")}
     poa(){return this.pos("absolute")}
     ovf(x){return this.addstyle("overflow", x)}
@@ -47,6 +59,17 @@ class ComponentProps_{
     ovfysc(){return this.ovfy("scroll")}
     cur(x){return this.addstyle("cursor", x)}
     cup(){return this.cur("pointer")}
+    drag(handler){
+        this.cur("move")
+        this.obj.draggable = true
+        this.obj.onDragStart = handler
+        this.obj.onDragEnter = handler
+        this.obj.onDragOver = handler
+        this.obj.onDragLeave = handler
+        this.obj.onDragEnd = handler
+        this.obj.onDrop = handler
+        return this
+    }
     get _(){return this.obj}
 }
 function p(objopt){return new ComponentProps_(objopt)}
--- resources/js/widgets.js
+++ resources/js/widgets.js
@@ -56,8 +56,10 @@ class EditableList extends TreeComponent{
     constructor(props){
         super(props)
 
-        this.width = this.props.width || 200
+        this.width = this.props.width || 250
         this.height = this.props.height || 20
+
+        this.dhc = this.props.dragHandleColor || "#00f"
 
         if(!this.state.options){
             this.state.options = []
@@ -102,17 +104,50 @@ class EditableList extends TreeComponent{
         this.setState(this.state)
     }
 
+    indexOfValue(value){
+        for(let i=0; i<this.state.options.length; i++) if(this.state.options[i][0] == value) return i
+        return null
+    }
+
+    optionDrag(value, ev){
+        ev.persist()
+        switch(ev.type){
+            case "dragstart":
+                this.draggedi = this.indexOfValue(value)
+                this.draggede = ev.target
+                this.draggede.style.backgroundColor = "#777"
+                break
+            case "dragenter": if(ev.target != this.draggede) ev.target.style.backgroundColor = "#ff0"; break
+            case "dragover": ev.preventDefault(); break
+            case "dragleave": if(ev.target != this.draggede) ev.target.style.backgroundColor = this.dhc; break            
+            case "drop":
+                ev.target.style.backgroundColor = this.dhc
+                this.draggede.style.backgroundColor = this.dhc
+                let dropi = this.indexOfValue(value)
+                this.state.options.itoj(this.draggedi, dropi)
+                this.build()
+                break
+        }
+    }
+
+    componentDidUpdate(){
+        this.selref.current.scrollIntoView({block: "center"})
+    }
+
     render(){        
-        return e('div', p({className: "unselectable"}).por().dib().bc("#aa0")._,
+        this.selref = React.createRef()
+
+        return e('div', p({className: "unselectable"}).ff("monospace").por().dib().bc(this.state.rolled ? "#77f" : "#bbb")._,
             e('div', p({}).dfc()._,
-                e('div', p({onClick: this.switchroll.bind(this)}).mar(2).ww(this.width).hh(this.height).pad(2).bc("#eee")._, this.state.selected ? this.state.selected : ""),
+                e('div', p({onClick: this.switchroll.bind(this)}).fs(this.height - 3).mar(2).ww(this.width).hh(this.height).pad(2).padl(4).bc("#eee")._, this.state.selected ? this.state.selected : ""),
                 e('button', p({onClick: this.switchroll.bind(this)}).mar(1).fs(this.height - 6)._, ">"),
                 e('button', p({onClick: this.add.bind(this)}).mar(1).fs(this.height - 6)._, "+"),
-                e('div', p({}).cup().bc("#aaf").ww(this.width + 100).ovfysc().poa().show(this.state.rolled).t(this.height + 8)._,
+                e('div', p({}).cup().bc("#aaf").mah(290).ww(this.width + 60).marl(1).ovfysc().poa().show(this.state.rolled).t(this.height + 8)._,
                     this.state.options.map(o=>
-                        e('div', p({key: "optionflex" + o[0]}).dfc()._,
-                            e('div', p({onClick: this.select.bind(this, o[0]),key: o[0]}).ww(this.width + 50).pad(1).bc(this.state.selected == o[0] ? "#7f7" : "#aff").mar(2)._, o[1]),
-                            e('button', p({onClick: this.delopt.bind(this, o[0])}).bc("#fcc")._, "X")
+                        e('div', p({ref: this.state.selected == o[0] ? this.selref : null, key: "optionflex" + o[0]}).dfc()._,
+                            e('div', p({}).ww(this.height - 4).hh(this.height - 4).bc(this.dhc).mar(2).marl(5).drag(this.optionDrag.bind(this, o[0]))._, null),
+                            e('div', p({onClick: this.select.bind(this, o[0]),key: o[0]}).ww(this.width - 14).pad(1).bc(this.state.selected == o[0] ? "#7f7" : "#aff").mar(2)._, o[1]),
+                            e('button', p({onClick: this.delopt.bind(this, o[0])}).fs(this.height - 9).marl(2).bc("#fcc")._, "X")
                         )
                     )
                 )
