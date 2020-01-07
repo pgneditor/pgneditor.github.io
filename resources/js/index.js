099e7abd8358be83a36f73500a6247d1635f4521
pgneditor <pgneditorapplication@gmail.com>
2020-01-07 08:01:46 +0100

    Show state

--- resources/js/index.js
+++ resources/js/index.js
@@ -7,9 +7,29 @@ class App extends TreeComponent{
         this.save()
     }
 
+    save(){
+        super.save()
+        if(this.textarearef)if(this.textarearef.current){
+            this.textarearef.current.value = JSON.stringify(this.state, null, 3)
+        }
+    }
+
+    parse(){
+        if(this.textarearef)if(this.textarearef.current){
+            this.state = JSON.parse(this.textarearef.current.value)
+            this.save()
+            this.setState(this.state)
+        }
+    }
+
     render(){
+        this.textarearef = React.createRef()
         return e('div', p({})._,
-            this.e(EditableList, p({id: "templates", width: 800, dontRollOnSelect: true})._, null)
+            this.e(EditableList, p({key: UID(), id: "templates", width: 800, dontRollOnSelect: true})._, null),
+            e('textarea', p({ref: this.textarearef, onChange: ()=>{}}).w(800).h(550)._, null),
+            e('div', {},
+                e('button', p({onClick: this.parse.bind(this)})._, "Parse")
+            )            
         )
     }
 }
